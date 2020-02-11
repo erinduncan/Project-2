331e68225ea4c479df2b06c5b3405ab92f620398
Erin Duncan <erinadduncan@gmail.com>
2020-02-10 19:05:38 -0500

    Various changes, POM ignore

--- Project2/src/main/java/com/revature/models/User.java
+++ Project2/src/main/java/com/revature/models/User.java
@@ -30,20 +30,54 @@ public class User {
 	private String lastName;
 	@OneToMany(mappedBy = "userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private Set<Post> posts;
+	@OneToMany(mappedBy="userId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private Set<Comments> comments;
+	
+
+	public Set<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(Set<Post> posts) {
+		this.posts = posts;
+	}
+
+	public Set<Comments> getComments() {
+		return comments;
+	}
+
+	public void setComments(Set<Comments> comments) {
+		this.comments = comments;
+	}
 
 	public User() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public User(String email, String password, String firstName, String lastName) {
+	public User(String email, String password, String firstName, String lastName, Set<Post> posts, Set<Comments> comments) {
 		super();
 		this.email = email;
 		this.password = password;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.posts = posts;
+		this.comments = comments;
 	}
-
+	
+	/**
+	 * 
+	 * @author erind
+	 * @param userId
+	 * @param email
+	 * @param password
+	 * @param firstName
+	 * @param lastName
+	 * 
+	 * This is effectively the register method; a new object is created with a userId of "0", which is automatically
+	 * overridden by the serialize function in SQL. Then the email, password, firstName, and lastName are added to the
+	 * table and a new user is created.
+	 */
 	public User(int userId, String email, String password, String firstName, String lastName) {
 		super();
 		this.userId = userId;
@@ -51,6 +85,17 @@ public class User {
 		this.password = password;
 		this.firstName = firstName;
 		this.lastName = lastName;
+	}
+
+	public User(int userId, String email, String password, String firstName, String lastName, Set<Post> posts, Set<Comments> comments) {
+		super();
+		this.userId = userId;
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.posts = posts;
+		this.comments = comments;
 	}
 
 	public int getUserId() {
@@ -96,7 +141,8 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", email=" + email + ", password=" + password + ", firstName=" + firstName
-				+ ", lastName=" + lastName + "]";
+				+ ", lastName=" + lastName + ", posts=" + posts + ", comments=" + comments + "]";
 	}
+
 
 }
