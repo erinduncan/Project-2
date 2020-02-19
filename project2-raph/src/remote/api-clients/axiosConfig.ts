5d984c68eac9d5d0e9a07dca4c3fb7038d016c20
maresh315 <mareshescoffery@gmail.com>
2020-02-19 11:48:10 -0500

    add a comment for example

--- project2-raph/src/remote/api-clients/axiosConfig.ts
+++ project2-raph/src/remote/api-clients/axiosConfig.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosConfig = axios.create({
     // baseURL: 'http://localhost:8080/Project-2/'
-    baseURL: 'http://3.81.82.21:2020/login'
+    baseURL: 'http://3.81.82.21:2020/login' //this is example api call username:MadisonRocks Password:password
 });
